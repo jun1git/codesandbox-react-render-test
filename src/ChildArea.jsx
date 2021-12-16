6f959c4df138d450664913c3edb13a305dcc23e7
jun1 <96167184+jun1git@users.noreply.github.com>
2021-12-16 18:43:11 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,26 @@
+import React, { memo, useState } from "react";
+
+const style = {
+  width: "100px",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildArea表示！");
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
